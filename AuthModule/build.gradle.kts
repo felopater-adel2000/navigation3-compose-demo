3899512f6b7e2e9146f3b1e1a15740505130989d
felopater-adel12 <felopater.sadek@maxab.io>
2026-04-03 18:23:43 +0200

    fix: update KSP plugin version in build configuration

--- AuthModule/build.gradle.kts
+++ AuthModule/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization)
-//    alias(libs.plugins.jetbrains.kotlin)
     id("com.google.devtools.ksp")
 }
 
